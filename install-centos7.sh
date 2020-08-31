d1261f93022f118b7b339c43516986de3a55d2c7
Gabriel Vergel <curious101@gmail.com>
2020-08-31 19:57:18 +0800

    added centos7 install script and dropbear fail2ban config

---
+++ install-centos7.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# set working directory
+mkdir /usr/local/src/underpass && cd /usr/local/src/underpass
+
+# update system
+yum -y update && yum -y upgrade
+
+# install EPEL repo
+yum -y install epel-release
+
+# install git
+yum install http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
+sed -i 's|enabled=1|enabled=0|' /etc/yum.repos.d/wandisco-git.repo
+yum --enablerepo=WANdisco-git -y install git
+
+# install system tools
+yum -y install screen nano wget curl ntp net-tools rsync glances htop ncdu
+
+# environment settings
+echo "include \"/usr/share/nano/sh.nanorc\"" > ~/.nanorc
+sed -i -e '$aexport VISUAL=nano' ~/.bashrc
+source ~/.bashrc
+
+# install dropbear - https://github.com/daybreakersx/premscript
+yum -y install dropbear
+echo "OPTIONS=\"-p 109 -p 110 -p 442\"" > /etc/sysconfig/dropbear
+echo "/bin/false" >> /etc/shells
+systemctl start dropbear
+systemctl enable dropbear
+
+# install cockpit
+yum -y install cockpit cockpit-dashboard
+
+# install fail2ban
+yum -y install fail2ban
+curl -o /etc/fail2ban/jail.local https://raw.githubusercontent.com/gabotronix/underpass/master/
+systemctl start fail2ban
+systemctl enable fail2ban
+
+# install wireguard - https://github.com/angristan/wireguard-install
+curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
+chmod +x wireguard-install.sh
+./wireguard-install.sh
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# disable ipv6
-sed -i -e '$anet.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
-sed -i -e '$anet.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.conf
-systemctl restart network
-
-#OpenVPN configuration
-sh builds/openvpn/setup.sh
