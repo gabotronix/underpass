87b82fc061e92c651a88264088f0674ee3a7413d
Gabriel Vergel <curious101@gmail.com>
2020-08-31 22:06:26 +0800

    added docker install

--- install-centos7.sh
+++ install-centos7.sh
@@ -12,7 +12,7 @@ yum -y install epel-release
 # install git
 yum install http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
 sed -i 's|enabled=1|enabled=0|' /etc/yum.repos.d/wandisco-git.repo
-yum --enablerepo=WANdisco-git install git -y
+yum -y --enablerepo=WANdisco-git install git
 
 # install system tools
 yum -y install screen nano wget ntp net-tools rsync glances htop ncdu
@@ -39,6 +39,18 @@ systemctl start cockpit.socket
 systemctl enable cockpit.socket
 firewall-cmd --zone=public --add-port=9090/tcp --permanent
 firewall-cmd --reload
+
+# install Docker and Docker Compose
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+
+# install Docker Compose - https://gist.githubusercontent.com/wdullaer/f1af16bd7e970389bad3/raw/4a5a72aece57e1deca926894e5919f90350c706d/install.sh
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
 # install fail2ban
 yum -y install fail2ban
