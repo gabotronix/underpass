841fa548ad7e79107c6aa9abd5991306b050ff6c
Gabriel Vergel <curious101@gmail.com>
2020-09-17 04:20:53 +0800

    install script in progress

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ function checkOS() {
 	elif [[ -e /etc/centos-release ]]; then
 		OS=centos
 	else
-		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, or CentOS system"
+		echo "Looks like you aren't running this installer on a Debian, Ubuntu, or CentOS system"
 		exit 1
 	fi
 }
@@ -41,65 +41,59 @@ function initialCheck() {
 }
 
 function installUnderpass() {
-	if [[ ${OS} == 'ubuntu' ]]; then
+	if [[ ${OS} == 'ubuntu' || ${OS} == 'debian' ]]; then
 		echo "Wireguard kernel modules are already installed."
-		#apt update
-		#apt install -y wireguard iptables resolvconf qrencode
 	elif [[ ${OS} == 'centos' ]]; then
-		curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-		yum -y install epel-release kernel kernel-devel kernel-headers
-		yum -y install wireguard-dkms wireguard-tools iptables qrencode
+		# update system
+		yum -y update && yum -y upgrade
+
+		# install EPEL repo and ELRepo
+		yum -y install epel-release elrepo-release
+
+		# install git
+		yum install http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
+		sed -i 's|enabled=1|enabled=0|' /etc/yum.repos.d/wandisco-git.repo
+		yum -y --enablerepo=WANdisco-git update git
+
+		# install system tools
+		yum -y install yum-utils screen nano wget ntp net-tools rsync glances htop ncdu
+
+		# install wireguard kernel modules
+		yum -y install yum-plugin-elrepo kmod-wireguard wireguard-tools
+
+		# environment settings
+		sed -i -e '$ainclude "/usr/share/nano/sh.nanorc"' ~/.nanorc
+		sed -i -e '$aexport VISUAL=nano' ~/.bashrc
+		source ~/.bashrc
+
+		# install fail2ban
+		yum -y install fail2ban
+		curl -o /etc/fail2ban/jail.local https://raw.githubusercontent.com/gabotronix/underpass/stage/config/fail2ban/jail-centos7.local
+		systemctl start fail2ban
+		systemctl enable fail2ban
 	fi
+
+		# install Docker
+		yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+		yum -y install docker-ce docker-ce-cli containerd.io
+		systemctl start docker
+		systemctl enable docker
+
+		# install Docker Compose - https://gist.githubusercontent.com/wdullaer/f1af16bd7e970389bad3/raw/4a5a72aece57e1deca926894e5919f90350c706d/install.sh
+		COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+		curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+		chmod +x /usr/local/bin/docker-compose
+		curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+
+		# install Underpass Docker apps
+		docker network create --subnet=172.20.0.0/24 underpass
+		cd /home/underpass
+		docker-compose up -d
+
+		# create non-root user for Docker
+		groupadd -g 1001 dockeru && useradd -u 1001 -g dockeru -G wheel,docker dockeru
+		sed -i -e '$a\%dockeru ALL=(ALL) ALL' /etc/sudoers
 }
-
-# update system
-yum -y update && yum -y upgrade
-
-# install EPEL repo and ELRepo
-yum -y install epel-release elrepo-release
-
-# install git
-yum install http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
-sed -i 's|enabled=1|enabled=0|' /etc/yum.repos.d/wandisco-git.repo
-yum -y --enablerepo=WANdisco-git update git
-
-# install system tools
-yum -y install yum-utils screen nano wget ntp net-tools rsync glances htop ncdu
-
-# install wireguard kernel modules
-yum -y install yum-plugin-elrepo kmod-wireguard wireguard-tools
-
-# environment settings
-sed -i -e '$ainclude "/usr/share/nano/sh.nanorc"' ~/.nanorc
-sed -i -e '$aexport VISUAL=nano' ~/.bashrc
-source ~/.bashrc
-
-# install Docker
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum -y install docker-ce docker-ce-cli containerd.io
-systemctl start docker
-systemctl enable docker
-
-# install Docker Compose - https://gist.githubusercontent.com/wdullaer/f1af16bd7e970389bad3/raw/4a5a72aece57e1deca926894e5919f90350c706d/install.sh
-COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
-curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
-
-# install fail2ban
-yum -y install fail2ban
-curl -o /etc/fail2ban/jail.local https://raw.githubusercontent.com/gabotronix/underpass/stage/config/fail2ban/jail-centos7.local
-systemctl start fail2ban
-systemctl enable fail2ban
-
-# install Underpass Docker apps
-docker network create --subnet=172.20.0.0/24 underpass
-cd /home/underpass
-docker-compose up -d
-
-# create non-root user for Docker
-groupadd -g 1001 dockeru && useradd -u 1001 -g dockeru -G wheel,docker dockeru
-sed -i -e '$a\%dockeru ALL=(ALL) ALL' /etc/sudoers
 
 # Check for root, OS...
 initialCheck
