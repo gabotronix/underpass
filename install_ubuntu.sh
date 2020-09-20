ffa3a0f270c6d23c0e70b8ef11662e5ad7482ac2
Gabriel Vergel <curious101@gmail.com>
2020-09-20 23:55:35 +0800

    ubuntu initial install script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 # The commands in this script are meant to be ran by root.
+
+# Add Ansible Repo
+apt update
+apt install -y software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install -y ansible git
+
+# Clone Repo
+git clone -b stage https://github.com/gabotronix/underpass.git /opt/underpass
+
+# Install Ansible Roles
+ansible-galaxy install -r /opt/underpass/requirements.yml
+
+# Run Ansible Playbook
+cd /opt/underpass
+ansible-playbook install.yml
+
+# Install Underpass
+docker network create underpass --subnet 172.20.0.0/24
+docker-compose up -d
