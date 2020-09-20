56d532fda97a2f559dedf46ef2d080e65c48fb44
Gabriel Vergel <curious101@gmail.com>
2020-09-21 01:19:51 +0800

    added web ui port info

--- install_centos7.sh
+++ install_centos7.sh
@@ -21,3 +21,14 @@ ansible-playbook -v install.yml
 # Install Underpass
 docker network create underpass --subnet 172.20.0.0/24
 docker-compose up -d
+
+# Enumerate Services and Ports
+MYIP=$(curl -4 icanhazip.com)
+clear
+echo " "
+echo "UNDERPASS INSTALLATION COMPLETE!"
+echo " "
+echo "Configure Pritunl VPN Web UI @ https://$MYIP:4433"
+echo "Configure Portainer @ http://$MYIP:9000"
+echo "Configure Heimdall @ http://$MYIP:85"
+echo "View Server Load @ http://$MYIP:19999"
