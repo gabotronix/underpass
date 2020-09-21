6dc47cb04f948b45a6fbec6510df6ec2d5a2f0e4
Gabriel Vergel <curious101@gmail.com>
2020-09-22 05:01:32 +0800

    removed upgrade from ubuntu shell script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,7 +4,6 @@
 
 # Add Ansible Repo and Install Ansible
 sudo apt update
-sudo apt upgrade -y
 sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible git
