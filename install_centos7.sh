3dcec2e73a7b640adf1e7c65e4a70fd697e95599
Gabriel Vergel <curious101@gmail.com>
2020-09-22 00:00:23 +0800

    fixed centos spacing

--- install_centos7.sh
+++ install_centos7.sh
@@ -36,26 +36,26 @@ UnderpassDir=`ls -l /opt | grep -c underpass`
 WhichDockerCompose=`which docker-compose | grep -c /usr`
 DockerPS=`sudo docker ps | grep -c mongodb`
 function countdown { #https://www.cyberciti.biz/faq/how-to-display-countdown-timer-in-bash-shell-script-running-on-linuxunix/
-        local OLD_IFS="${IFS}"
-        IFS=":"
-        local ARR=( $1 )
-        local SECONDS=$((  (ARR[0] * 60 * 60) + (ARR[1] * 60) + ARR[2]  ))
-        local START=$(date +%s)
-        local END=$((START + SECONDS))
-        local CUR=$START
+    local OLD_IFS="${IFS}"
+    IFS=":"
+    local ARR=( $1 )
+    local SECONDS=$((  (ARR[0] * 60 * 60) + (ARR[1] * 60) + ARR[2]  ))
+    local START=$(date +%s)
+    local END=$((START + SECONDS))
+    local CUR=$START
 
-        while [[ $CUR -lt $END ]]
-        do
-                CUR=$(date +%s)
-                LEFT=$((END-CUR))
+    while [[ $CUR -lt $END ]]
+    do
+            CUR=$(date +%s)
+            LEFT=$((END-CUR))
 
-                printf "\r%02d:%02d:%02d" \
-                        $((LEFT/3600)) $(( (LEFT/60)%60)) $((LEFT%60))
+            printf "\r%02d:%02d:%02d" \
+                    $((LEFT/3600)) $(( (LEFT/60)%60)) $((LEFT%60))
 
-                sleep 1
-        done
-        IFS="${OLD_IFS}"
-        echo "        "
+            sleep 1
+    done
+    IFS="${OLD_IFS}"
+    echo "        "
 }
 
 function webpanels() {
