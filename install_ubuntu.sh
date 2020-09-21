ae76f79b97878762476aa45e578964e4b0396578
Gabriel Vergel <curious101@gmail.com>
2020-09-22 04:52:59 +0800

    moved netdata to core

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -66,6 +66,7 @@ function webpanels() {
     echo -e "===================================================="
     echo -e "\nConfigure Portainer @ http://$PublicIP:9000\n"
     echo -e "Configure Pritunl VPN @ https://$PublicIP:4433\n"
+    echo -e "View Server Load @ http://$PublicIP:19999\n"
     echo -e "----------------------------------------------------"
 }
 
