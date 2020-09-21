28b8dc3f2b30d225e25204949a7e2ee541d02f07
Gabriel Vergel <curious101@gmail.com>
2020-09-21 21:10:30 +0800

    added sleep

--- install_centos7.sh
+++ install_centos7.sh
@@ -39,6 +39,8 @@ elif [ $DockerPS != 1 ]; then
     echo -e "Installation failed. Please run the installer again."
     exit 1
 else
+    echo -e "Initializing Containers..."
+    sleep 20
     PublicIP=$(curl -4 ifconfig.co 2>/dev/null)
     echo -e "\n\n===================================================="
     echo -e "Configure Your Underpass Web Panels:"
