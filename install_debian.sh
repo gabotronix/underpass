e5a9403415d52601cc44e53716bd049062bf3dcf
Gabriel Vergel <curious101@gmail.com>
2020-11-22 11:33:10 +0800

    removed ansible repo

--- install_debian.sh
+++ install_debian.sh
@@ -5,7 +5,6 @@
 # Add Ansible Repo and Install Ansible
 sudo apt update
 sudo apt install -y software-properties-common
-#sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible git
 
 # Upgrade iptables
