10ad64fc34a78985ae77c133f30ce29c84e63ee4
Gabriel Vergel <curious101@gmail.com>
2020-11-22 09:37:05 +0800

    disabled ansible repo

--- install_rpi.sh
+++ install_rpi.sh
@@ -6,7 +6,7 @@
 apt install sudo -y
 sudo apt update
 sudo apt install -y software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
+#sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible git nftables python
 
 # Clone Repo
