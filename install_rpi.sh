5d45363f262ff04dcf8aacb8c54da834bd6504ee
Gabriel Vergel <curious101@gmail.com>
2020-11-22 11:55:54 +0800

    added buster backports

--- install_rpi.sh
+++ install_rpi.sh
@@ -6,7 +6,6 @@
 apt install sudo -y
 sudo apt update
 sudo apt install -y software-properties-common
-#sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible git python
 
 # Clone Repo
